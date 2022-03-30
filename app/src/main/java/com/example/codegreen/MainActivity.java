4c423867b2bb3200a345ade6d097d2f06133c079
Efren Vasquez <efrenvasquez@u.boisestate.edu>
2022-03-30 00:01:11 -0500

    updated files

--- app/src/main/java/com/example/codegreen/MainActivity.java
+++ app/src/main/java/com/example/codegreen/MainActivity.java
@@ -1,16 +1,25 @@
 package com.example.codegreen;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import com.example.codegreen.ui.userprofile.UserProfileFragment;
+import com.example.codegreen.ui.userprofile.UserProfileViewModel;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+
+
 import com.example.codegreen.databinding.ActivityMainBinding;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,11 +36,37 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_leaderboard, R.id.navigation_footprint, R.id.navigation_overview, R.id.navigation_scan)
+                R.id.navigation_home, R.id.navigation_leaderboard, R.id.navigation_footprint, R.id.navigation_overview, R.id.navigation_scan, R.id.navigation_userprofile, R.id.navigation_settings)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.top_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.user_profile_nav:
+                setContentView(R.layout.fragment_userprofile);
+                return true;
+
+            case R.id.settings_nav:
+                setContentView(R.layout.fragment_settings);
+                return true;
+
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+
+
 }
+
